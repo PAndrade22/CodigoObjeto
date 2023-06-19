38999350d1e6db7fc3ae1ff725227a3250064e11
PAndrade22 <131625628+PAndrade22@users.noreply.github.com>
2023-06-19 00:11:36 -0300

    UordiOfiUorcefiti.cs

--- UordiOfiUorcefiti.cs
+++ UordiOfiUorcefiti.cs
@@ -1,13 +1,13 @@
 using System;
 namespace UordiofUorcrefiti
 {
-  public class Heroi 
+  public abstract class Heroi 
   {
     private string nomeHeroi;
-    private int pontosDeVida;
+    private float pontosDeVida;
 
     //Construtor Herói
-    Heroi(string nomeHeroi, int pontosDeVida)
+    Heroi(string nomeHeroi, float pontosDeVida)
     {
       this.nomeHeroi = nomeHeroi;
       this.pontosDeVida = pontosDeVida;
@@ -16,51 +16,126 @@ namespace UordiofUorcrefiti
     public string getnomeHeroi(){
       return (nomeHeroi);
     }
-    public int getpontosDeVida(){
+    public float getpontosDeVida(){
       return (pontosDeVida);
     }
     //Método Set
     public void setnomeHeroi(string nomeHeroi){
       this.nomeHeroi = nomeHeroi;
     }
-    public void setpontosDeVida(int pontosDeVida){
+    public void setpontosDeVida(float pontosDeVida){
       this.pontosDeVida = pontosDeVida;
     }
+    //Método Lançar Magia 
+    public abstract class LancarMagia()
+    {
+
+    }
+    //Método Atacar com Arma
+    public abstract class AtacarComArma()
+    {
+
+    }
     //Método Reduzir pontos de vida
-    public double reduzirVida(int pontosDeVida)
+    public double reduzirVida(float pontosDeVida)
     {
       int reduzirPontosDeVida = 5;
       this.pontosDeVida -= reduzirPontosDeVida;
       return pontosDeVida;
     }
-     public double aumentarVida(int pontosDeVida)
+     public double aumentarVida(float pontosDeVida)
     {
       int aumentarPontosDeVida = 8;
       this.pontosDeVida += aumentarPontosDeVida;
       return pontosDeVida;
     }
+    //Classe Herói da Aliança
     public class HeroiAlianca : Heroi
     {
-
+     //Construtor
+     HeroiAlianca(string nomeHeroi, float pontosDeVida) : base (nomeHeroi, pontosDeVida)
+     {
+      this.nomeHeroi = nomeHeroi;
+      this.pontosDeVida = pontosDeVida;
+     }
+     public double reduzirVida(float pontosDeVida)
+    { 
+      float bonus = 0.2f;
+      int reduzirPontosDeVida = 5;
+      this.pontosDeVida -= reduzirPontosDeVida + bonus;
+      return pontosDeVida;
+    }
+     public double aumentarVida(int pontosDeVida)
+    {
+      float bonus = 0.2f;
+      int aumentarPontosDeVida = 8;
+      this.pontosDeVida += aumentarPontosDeVida + bonus;
+      return pontosDeVida;
+    }
+    public class HeroiDaHorda : Heroi
+    {
+      HeroiDaHorda(string nomeHeroi, float pontosDeVida) : base (nomeHeroi, pontosDeVida)
+      {
+        this.nomeHeroi = nomeHeroi;
+        this.pontosDeVida = pontosDeVida;
+      }
+     public double reduzirVida(float pontosDeVida)
+    { 
+      float bonus = 0.14f;
+      int reduzirPontosDeVida = 5;
+      this.pontosDeVida -= reduzirPontosDeVida + bonus;
+      return pontosDeVida;
     }
     
-    public class ProgramaPrincipal
+     public double aumentarVida(int pontosDeVida)
     {
+      float bonus = 0.14f;
+      int aumentarPontosDeVida = 8;
+      this.pontosDeVida += aumentarPontosDeVida + bonus;
+      return pontosDeVida;
+     }
+
+    
+    public class ProgramaPrincipal
+    { 
   
     public static void Main(string[] args)
     {
       string nomeHeroi = "";
-      int pontosDeVida = 100;
+      float pontosDeVida = 100.0f;
 
-      Heroi Hero = new Heroi(nomeHeroi = "Zymmax", pontosDeVida);
-      Console.WriteLine("O nome do herói é {0}", Hero.getnomeHeroi());
-      Console.WriteLine("Os pontos de vida do 'Herói' {0} é {1} pontos ", Hero.getnomeHeroi(), Hero.getpontosDeVida());
+      //Heroi Hero = new Heroi(nomeHeroi = "Zymmax", pontosDeVida);
+      //Console.WriteLine("O nome do herói é {0}", Hero.getnomeHeroi());
+      //Console.WriteLine("Os pontos de vida do 'Herói' {0} é {1} pontos ", Hero.getnomeHeroi(), Hero.getpontosDeVida());
       
-      Hero.reduzirVida(pontosDeVida);
-      Console.WriteLine("Os pontos de vida do 'Heóri' foi reduzido para {0}", Hero.getpontosDeVida());
-      Hero.aumentarVida(pontosDeVida);
-      Console.WriteLine("Os pontos de vida do 'Herói' foi aumentado para {0}.", Hero.getpontosDeVida());
-   }
-  } 
+      //Hero.reduzirVida(pontosDeVida);
+      //Console.WriteLine("Os pontos de vida do 'Heóri' foi reduzido para {0}", Hero.getpontosDeVida());
+      //Hero.aumentarVida(pontosDeVida);
+      //Console.WriteLine("Os pontos de vida do 'Herói' foi aumentado para {0}.", Hero.getpontosDeVida());
+      //Console.WriteLine("");
+
+      
+      HeroiAlianca HeroAlianca = new HeroiAlianca(nomeHeroi = "Zadur", pontosDeVida);
+      Console.WriteLine("O nome do herói é {0}", HeroAlianca.getnomeHeroi());
+      Console.WriteLine("Os pontos de vida do 'Herói' {0} é {1} pontos ", HeroAlianca.getnomeHeroi(), HeroAlianca.getpontosDeVida());
+      
+      HeroAlianca.reduzirVida(pontosDeVida);
+      Console.WriteLine("Os pontos de vida do 'Heóri' foi reduzido para {0}", HeroAlianca.getpontosDeVida());
+      HeroAlianca.aumentarVida(pontosDeVida);
+      Console.WriteLine("Os pontos de vida do 'Herói' foi aumentado para {0}.", HeroAlianca.getpontosDeVida());
+      Console.WriteLine("");
+      
+      HeroiDaHorda HeroHorda = new HeroiDaHorda(nomeHeroi = "Zadur", pontosDeVida);
+      Console.WriteLine("O nome do herói é {0}", HeroHorda.getnomeHeroi());
+      Console.WriteLine("Os pontos de vida do 'Herói' {0} é {1} pontos ", HeroHorda.getnomeHeroi(), HeroHorda.getpontosDeVida());
+      
+      HeroHorda.reduzirVida(pontosDeVida);
+      Console.WriteLine("Os pontos de vida do 'Heóri' foi reduzido para {0}", HeroHorda.getpontosDeVida());
+      HeroHorda.aumentarVida(pontosDeVida);
+      Console.WriteLine("Os pontos de vida do 'Herói' foi aumentado para {0}.", HeroHorda.getpontosDeVida());
+       }
+      }
+     }
+    }
+   } 
  }
-}
